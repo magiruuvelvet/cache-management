3e4490f0b6b9e96e71d3755658d64b99ef767619
magiruuvelvet <magiruuvelvet@naver.com>
2023-10-06 10:58:59 +0200

    libcachemgr: fix mistake with mutex locking

--- src/libcachemgr/libcachemgr.cpp
+++ src/libcachemgr/libcachemgr.cpp
@@ -17,21 +17,24 @@ namespace {
     static std::mutex user_configuration_mutex;
 } // anonymous namespace
 
+/// alias the type of the mutex lock mechanism for easier switching between them
+using mutex_lock_t = std::lock_guard<std::mutex>;
+
 user_configuration_t *user_configuration_t::instance()
 {
-    std::lock_guard{user_configuration_mutex};
+    mutex_lock_t lock{user_configuration_mutex};
     static user_configuration_t instance;
     return &instance;
 }
 
 void user_configuration_t::set_configuration_file(const std::string &configuration_file) noexcept
 {
-    std::lock_guard{user_configuration_mutex};
+    mutex_lock_t lock{user_configuration_mutex};
     this->_configuration_file = configuration_file;
 }
 
 const std::string &user_configuration_t::configuration_file() const noexcept
 {
-    std::lock_guard{user_configuration_mutex};
+    mutex_lock_t lock{user_configuration_mutex};
     return this->_configuration_file;
 }
