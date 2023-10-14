1e6074b78a79ddf5d55059eb6f50ae0afafba1ab
magiruuvelvet <magiruuvelvet@naver.com>
2023-10-14 15:03:16 +0200

    libcachemgr/pm: update comment regarding cargo caches

--- src/libcachemgr/package_manager_support/cargo/cargo.hpp
+++ src/libcachemgr/package_manager_support/cargo/cargo.hpp
@@ -16,7 +16,16 @@ public:
      * cargo cache lookup:
      *
      * `$CARGO_HOME` contains more than just caches.
-     * Reference: https://doc.rust-lang.org/cargo/guide/cargo-home.html
+     *
+     * References:
+     *  - https://doc.rust-lang.org/cargo/guide/cargo-home.html
+     *  - https://github.com/rust-lang/cargo/issues/10252
+     *  - https://github.com/rust-lang/cargo/issues/1734
+     *  - https://poignardazur.github.io/2023/05/23/platform-compliance-in-cargo/
+     *
+     * locations which are definitely cache and safe to delete for any reason:
+     *  - `$CARGO_HOME/registry` - local crates registry and source code
+     *  - `$CARGO_HOME/git`      - cloned git repositories
      *
      * `$CARGO_HOME` defaults to `$HOME/.cargo` if not set.
      *
