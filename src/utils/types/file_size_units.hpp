5401494023bf525b4a2f03c422323e491dbda5ab
magiruuvelvet <magiruuvelvet@naver.com>
2023-10-14 18:49:19 +0200

    utils: further improve human_readable_file_size

--- src/utils/types/file_size_units.hpp
+++ src/utils/types/file_size_units.hpp
@@ -1,17 +1,11 @@
 #pragma once
 
 #include <ostream>
-//#include <iomanip>
+#include <iomanip>
 #include <cinttypes>
 #include <cmath>
 
 namespace file_size_units {
-
-enum class file_size_unit : unsigned
-{
-    kibi = 1024,
-    kilo = 1000,
-};
 
 /**
  * `std::ostream` helper to print a file size in a human-readable format.
@@ -32,33 +26,45 @@ struct human_readable_file_size final
     using size_type = std::uintmax_t;
     using decimal_type = unsigned;
 
+    enum class file_size_unit : size_type
+    {
+        kibi = 1024,
+        kilo = 1000,
+    };
+
     explicit human_readable_file_size(size_type size)
-        : size(size), decimal_places(2), unit(double(file_size_unit::kilo))
+        : size(size), decimal_places(2), unit(file_size_unit::kilo), unit_d(double(file_size_unit::kilo))
     {}
 
     explicit human_readable_file_size(size_type size, decimal_type decimal_places)
-        : size(size), decimal_places(decimal_places), unit(double(file_size_unit::kilo))
+        : size(size), decimal_places(decimal_places), unit(file_size_unit::kilo), unit_d(double(file_size_unit::kilo))
     {}
 
     explicit human_readable_file_size(size_type size, decimal_type decimal_places, file_size_unit unit)
-        : size(size), decimal_places(decimal_places), unit(double(unit))
+        : size(size), decimal_places(decimal_places), unit(unit), unit_d(double(unit))
     {}
 
 private:
     const size_type size;
     const decimal_type decimal_places;
-    const double unit;
+    const file_size_unit unit;
+    const double unit_d;
+
+    static constexpr const auto steps = "BKMGTPE";
 
     friend std::ostream &operator<<(std::ostream &os, const human_readable_file_size &hr)
     {
-        int o{};
+        int step{};
         double mantissa = hr.size;
-        for (; mantissa >= hr.unit; mantissa /= hr.unit, ++o);
+        for (; mantissa >= hr.unit_d; mantissa /= hr.unit_d, ++step);
         const auto pow = std::pow(10, hr.decimal_places);
         const double rounded = std::round(mantissa * pow) / pow;
-        os << rounded << "BKMGTPE"[o];
-        //os << std::fixed << std::setprecision(hr.decimal_places) << std::setfill('0') << rounded << "BKMGTPE"[o];
-        return o ? os << 'B' : os;
+        if (hr.size < size_type(hr.unit)) {
+            os << rounded << steps[step];
+        } else {
+            os << std::fixed << std::setprecision(hr.decimal_places) << std::setfill('0') << rounded << steps[step];
+        }
+        return step ? os << steps[0] : os; // ensure this is always 'B' in case you refactor {steps}
     }
 };
 
