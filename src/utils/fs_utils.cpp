d7f6fe709dde96056b13213524c3d6bd185a2fb9
magiruuvelvet <magiruuvelvet@naver.com>
2023-10-15 12:07:24 +0200

    utils: implement convenient line-by-line file reader

--- src/utils/fs_utils.cpp
+++ src/utils/fs_utils.cpp
@@ -38,4 +38,36 @@ std::string read_text_file(std::string_view path, std::error_code *ec) noexcept
     return buffer;
 }
 
+std::error_code find_in_text_file(std::string_view path, std::string &out,
+    std::function<bool(std::string_view line, std::string &cb_out)> line_callback) noexcept
+{
+    std::ifstream fstream(path, std::ios::in);
+    if (!fstream)
+    {
+        // TODO: use <format> once it is more available across different C++ standard libaries
+        logging_helper::get_logger()->log_error("failed to open file: " + std::string{path} +
+            " (" + strerror(errno) + ")");
+        return std::make_error_code(std::errc{errno});
+    }
+
+    if (fstream.is_open())
+    {
+        std::string line;
+        while (std::getline(fstream, line))
+        {
+            std::string cb_out;
+            if (line_callback(line, cb_out))
+            {
+                out = cb_out;
+                break;
+            }
+        }
+
+        fstream.close();
+    }
+
+    // empty error code indicates success
+    return std::error_code{};
+}
+
 } // namespace fs_utils
--- src/utils/fs_utils.hpp
+++ src/utils/fs_utils.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <system_error>
+#include <functional>
 
 namespace fs_utils {
 
@@ -16,5 +18,28 @@ namespace fs_utils {
  * @return file contents as a string
  */
 std::string read_text_file(std::string_view path, std::error_code *ec = nullptr) noexcept;
+
+/**
+ * Reads a text file line-by-line and invokes the given @p line_callback function for each line.
+ *
+ * The line callback function receives a string view to the line which is currently being processed.
+ *
+ * If you found the data you need in the line, the callback function should return `true` to
+ * indicate that further processing should be stopped. The found (sub)string should be written to
+ * the `cb_out` parameter of the callback function. Once the search is complete, your (sub)string
+ * will be written to the @p out paramter of this function.
+ *
+ * Your callback function should not throw exceptions. If you are calling throwable code inside
+ * your callback function, you should catch and handle all expections internally.
+ *
+ * Errors are returned as `std::error_code` and are sent to the logger.
+ *
+ * @param path path to the file to read
+ * @param out the found (sub)string
+ * @param line_callback `bool(std::string_view line, std::string &cb_out)`
+ * @return error code
+ */
+std::error_code find_in_text_file(std::string_view path, std::string &out,
+    std::function<bool(std::string_view line, std::string &cb_out)> line_callback) noexcept;
 
 } // namespace fs_utils
